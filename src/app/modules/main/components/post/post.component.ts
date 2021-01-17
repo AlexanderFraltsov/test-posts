bf0425a98deff4b94db17f7ec16440a731f2e431
AlexanderFraltsov <kolonessens@yandex.ru>
2021-01-17 21:12:16 +0400

    feat: modify post implemented

--- src/app/modules/main/components/post/post.component.ts
+++ src/app/modules/main/components/post/post.component.ts
@@ -1,5 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { IPost } from '../../../../shared/models/post.model';
+import { PostService } from '../../services/post.service';
+
+const requiredFileType = (type: string) => (control: FormControl) => {
+  const file = control.value;
+  if (file) {
+    if (file.name) {
+      const ext = file.name.split('.')[1].toLowerCase();
+      if (type.toLowerCase() === ext) {
+        return null
+      }
+    }
+  }
+  return {
+    requiredFileType: true
+  }
+}
 
 @Component({
   selector: 'app-post',
@@ -8,9 +27,49 @@ import { IPost } from '../../../../shared/models/post.model';
 })
 export class PostComponent implements OnInit {
   @Input() public post?: IPost;
-  constructor() { }
+  constructor(
+    private postService: PostService,
+    private modalService: NgbModal
+  ) { }
+
+  public postForm: FormGroup;
+  public formControls = {
+    text: new FormControl('', [
+      Validators.required
+    ]),
+    file: new FormControl(null, [
+      requiredFileType('pdf')
+    ]),
+  };
 
   ngOnInit(): void {
+    this.postForm = new FormGroup(this.formControls);
   }
 
+  public open(content: any) {
+    this.modalService
+      .open(content, {ariaLabelledBy: 'modal-basic-title'})
+      .result
+      .then(
+        result => {
+          if (result === 'remove') {
+            this.onDelete();
+          }
+          if (result === 'edit') {
+            this.onEdit();
+          }
+        },
+        reason => {}
+      )
+  }
+
+  public onDelete() {
+    this.postService.removePost(this.post!.id);
+  }
+
+  public onEdit() {
+    console.log(this.postForm.value);
+    const { file, text } = this.postForm.value;
+    this.postService.updatePost({ id: this.post!.id, text, isModified: true});
+  }
 }
--- src/app/modules/main/services/post.service.ts
+++ src/app/modules/main/services/post.service.ts
@@ -1,20 +1,22 @@
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { IPost } from 'src/app/shared/models/post.model';
+
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 const mockPosts: IPost[] = [
   {
     id: '1',
     author: 'User 1',
     date: new Date(),
-    text: 'text1',
+    text: lorem,
     isModified: false
   },
   {
     id: '2',
     author: 'User 2',
     date: new Date(),
-    text: 'text2',
+    text: lorem,
     isModified: true
   }
 ];
@@ -23,10 +25,30 @@ const mockPosts: IPost[] = [
   providedIn: 'root'
 })
 export class PostService {
-
+  public posts$: BehaviorSubject<IPost[]> = new BehaviorSubject<IPost[]>(mockPosts);
   constructor() { }
 
   public getAll(): Observable<IPost[]> {
-    return new Observable((subscriber) => subscriber.next(mockPosts));
+    return this.posts$;
+  }
+
+  public addPost(post: IPost): void {
+    const prev = this.posts$.getValue();
+    this.posts$.next([...prev, post]);
+  }
+
+  public removePost(id: string): void {
+    const prev = this.posts$.getValue();
+    this.posts$.next(prev.filter(post => post.id !== id));
+  }
+
+  public updatePost(post: Partial<IPost>): void {
+    const prev = this.posts$.getValue();
+    this.posts$.next(prev.map(el => {
+      if (el.id === post.id) {
+        return {...el, ...post};
+      }
+      return el;
+    }));
   }
 }
